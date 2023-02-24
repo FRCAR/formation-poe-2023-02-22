bcb0db9aafdf864a49c331b205b27b870fc57b6d
François Caremoli <francois.caremoli@wanadoo.fr>
2023-02-24 10:55:15 +0100

    Ajout du cours sur les boucles

--- com/bigcorp/formation/cours/Boucles.java
+++ com/bigcorp/formation/cours/Boucles.java
@@ -5,12 +5,35 @@ public class Boucles {
 	public static void main(String[] args) {
 		// Boucle for
 		System.out.println("\r\n boucle for");
+		for (int index = 5; index <= 9; index++) {
+			System.out.println("index vaut : " + index);
+		}
 
 		// Boucle do ... while
 		System.out.println("\r\n  boucle do while");
+		int compteur = 0;
+		do {
+			System.out.println("coucou");
+			compteur++;
+		} while (compteur < 10);
+
+		// Boucle while
+		System.out.println("\r\n  boucle while");
+		boolean aFaitUneBoucle = false;
+		while (!aFaitUneBoucle) {
+			System.out.println("Je boucle avec while");
+			System.out.println("coucou");
+		}
 
 		// Boucle while avec break et continue
-		System.out.println("\r\n boucle while");
+		while (true) {
+			System.out.println("Je vais afficher ce message une fois à cause du break");
+			break;
+		}
+
+
+
+		// System.out.println("\r\n boucle while");
 
 	}
 }
